ada292749caa7d688380459c7b4ea5f1f62c90fe
Orasta Mirdadoeva <orasta@silkroadprofessionals.com>
2023-09-28 16:57:10 +0500

    added first lesson

---
+++ projects/lesson1/lesson1/Program.cs
@@ -0,0 +1,7 @@
+﻿// See https://aka.ms/new-console-template for more information
+using System.Threading.Channels;
+
+Console.WriteLine("Hello, Where are you from?");
+
+string country = Console.ReadLine();
+Console.WriteLine($"{country}");
