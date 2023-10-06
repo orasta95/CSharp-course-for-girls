d66ccb9e089f41acd834ba65672abb6a9a5b3221
Orasta Mirdadoeva <94688630+orasta95@users.noreply.github.com>
2023-10-06 14:07:56 +0500

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,16 @@
+﻿// See https://aka.ms/new-console-template for more information
+using System.ComponentModel.Design;
+using System.Net.Http.Headers;
+using System.Runtime.CompilerServices;
+
+string name = "Book";
+switch (name)
+{ case "Pen";
+        Co
+"
+
+
+}
+
+
+
